a86ff37fc0a0daf713ed5d389205f3f07bb8d0fb
lee jaehoon <jaehoonlee88@gmail.com>
2013-05-01 22:07:53 +0900

    05.01

--- knowledgeShare/settings.py
+++ knowledgeShare/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/leejaehoon/Documents/KnowShare/knowledgeShare/knowShareWeb/templates",
+    "/Users/leejaehoon/Documents/Development/PythonProjects/knowledgeShare/knowShareWeb/templates",
 )
 
 # List of finder classes that know how to find static files in
